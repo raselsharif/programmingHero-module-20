c9535609a362bc7b704363cc2a8c5242fb6206da
raselsharif <raselsharif9@gmail.com>
2023-08-02 23:18:43 +0600

    added let-const

---
+++ let-const.js
@@ -0,0 +1,17 @@
+// let can change variable value but not re-declare but can change value.
+
+let myName ="Rasel";
+
+// let myName ="Sharif": 
+myName = "Sharif";
+
+console.log(myName);
+
+
+// const can not change value nor can not re-declare
+
+const id = 1771;
+
+// id=1881;
+
+console.log(id);
