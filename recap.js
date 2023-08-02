123bb3319c83dc2e88c5ea6cd86b1f89da448024
raselsharif <raselsharif9@gmail.com>
2023-08-02 23:14:19 +0600

    added recap

---
+++ recap.js
@@ -0,0 +1,43 @@
+// variable
+var myName= "Rasel Sharif";
+
+// array
+var arr =[20,10,30,60,50,40];
+
+// condition
+if(10>50){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
+
+// loop
+
+var i =0;
+
+while(i<=10){
+    console.log(i);
+    i++;
+}
+console.log("===============");
+for(var i=0; i<=10;i++){
+    console.log(i);
+}
+
+// function
+
+function number (number){
+    console.log(number);
+}
+
+number(1000)
+
+// Object
+
+var student ={
+    name: "rasel",
+    id:1771,
+    address: "Badda"
+}
+console.log(student);
