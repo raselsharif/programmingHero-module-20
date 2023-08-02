18f19ffbc73b413e03dff792d26569e4ecd4b99c
raselsharif <raselsharif9@gmail.com>
2023-08-03 00:29:28 +0600

    added is even

---
+++ isEven.js
@@ -0,0 +1,13 @@
+
+
+function isEven (number){
+const remainder = number%2;
+   if(remainder === 0){
+    return true;
+   }else{
+    return false;
+   }
+}
+
+const myNumber = isEven(12);
+console.log("Is it even: ", myNumber);
